cee801de14ce845c0094592e4e579e3fd705f0ca
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-11-26 18:20:11 +0500

    Initial Commit

---
+++ M5_L6_Task3&4/M5_L6_Task3_4App.swift
@@ -0,0 +1,20 @@
+//
+//  M5_L6_Task3_4App.swift
+//  M5_L6_Task3&4
+//
+//  Created by Bekhruz Hakmirzaev on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct M5_L6_Task3_4App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
